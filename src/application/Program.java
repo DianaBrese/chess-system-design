3c4760b8e065ac2388baaef597da3223dd0e12c1
Diana <dianabrese15@gmail.com>
2024-01-19 11:48:39 -0300

    First class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boargame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-System.out.println("Hello");
+		Position position = new Position(3,5);
+		System.out.println(position);
 	}
 
 }
